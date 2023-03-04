21f646c74106f926591d66a523c50a130d18fe7f
Andrey Kerges <andrey.nascimento4@etec.sp.gov.br>
2023-03-03 22:11:09 -0300

    finaliza exercicios FrutasPreferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Minhas Frutas preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1. Manga");
+Console.WriteLine("2. Morango");
+Console.WriteLine("3. Banana");
+Console.WriteLine("4. Pera");
+Console.WriteLine("5. Uva");
